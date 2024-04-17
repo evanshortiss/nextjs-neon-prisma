cab2505e0bd6640be75bb51ef503d25c6412e35b
Evan Shortiss <evanshortiss@gmail.com>
2024-04-17 16:29:38 -0700

    chore: add second query to test pool

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,14 @@ import { db } from "@/db";
 export const dynamic = 'force-dynamic'
 
 async function getUsers() {
-  return db.user.findMany();
+  return Promise.all([
+    db.user.findMany(),
+    db.post.count()
+  ])
 }
 
 export default async function Home() {
-  const users = await getUsers();
+  const [users, posts] = await getUsers();
 
   const userEls = users.map((user) => {
     return (
@@ -17,7 +20,9 @@ export default async function Home() {
     )
   })
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center p-24">
+      <p>Post count: {posts}</p>
+      <hr />
       <ul>
         {userEls}
       </ul>
